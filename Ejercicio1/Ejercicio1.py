f9ff1e1abfca39de00a497f0716709268906e542
AnDumu <an.dumu@hotmail.com>
2021-02-20 01:24:44 +0100

    estructura

---
+++ Ejercicio1/Ejercicio1.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+#resultados a retornar en formato clase (enum)
+class Numero(Enum):
+    PERFECTO = 1
+    ABUNDANTE = 2
+    DEFECTIVO = 3
+
+def Ejercicio1(zNum):
+	pass
+
+Ejercicio1(12)
